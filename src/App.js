ddcf65626771c4937f593d1fead4d1858b04a050
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-12 22:11:07 +0600

    initialization react app with daisyui

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import daisyui from './daisyui-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        +
+        <img src={daisyui} className="" alt="logo" />
+        <p>
+          React.JS + Tailwind CSS + DaisyUI Configuration Template.
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default App;
